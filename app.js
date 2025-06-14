b09bd8c58636f7682015e2855e40a2d7b37ef6fd
Marc Ohlmann <marc.ohlmann25@gmail.com>
2025-06-14 09:19:35 -0700

    added ejs and a index view

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
 
-app.get("/", (request, response) => response.send("Hello, world!"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (request, response) => response.render("index", { message: "Hello world!" }));
 
 const PORT = 3000;
 app.listen(PORT, () => {
