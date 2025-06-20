39753dba45ed9ddaa188c59c61feef61f6140602
Marc Ohlmann <marc.ohlmann25@gmail.com>
2025-06-20 12:54:30 -0700

    added dotenv support for environment variables to be used for sensitive info in a hidden local file called .env

--- db/pool.js
+++ db/pool.js
@@ -1,14 +1,11 @@
 const { Pool } = require("pg");
 
 
-// The following should be read from environment variables
-// We're hardcoding them here for simplicity
-const connectionString = "postgresql://marco:chilidog5@localhost:5432/top_users";
-
-module.exports =  {
-
-    connectionString: connectionString,
-    pool: new Pool({ connectionString: connectionString }),
-
-}
+module.exports =  new Pool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT
+});
 
